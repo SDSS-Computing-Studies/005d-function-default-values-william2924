e0e94dd6d6a33cf77238fbb9e5e482c31c203f0c
william2924 <78515221+william2924@users.noreply.github.com>
2021-03-11 16:20:11 -0800

    all

--- task2.py
+++ task2.py
@@ -14,10 +14,10 @@ assert multiplication(5) == [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
 assert multiplication(2,5) == [2, 4, 6, 8, 10]
 """
 def multiplication(num,t=12):
-    lst=[]
-    nth=0
+    list=[]
+    x=0
     for i in range(t):
-        nth=nth+1
-        nums=num*nth
-        lst.append(nums)
-        return lst
+        x=x+1
+        y=num*x
+        list.append(t)
+        return list
--- task3.py
+++ task3.py
@@ -14,6 +14,6 @@ assert title() == "=========\n= Title =\n========="
 (2 points)
 
 """
-def title(symbolt = "="):
-    output = symbol * 9 + "\n" + symbol + " Title " + symbol + "\n" + symbol * 9
+def title(symbol="="):
+    output=symbol*9+"\n"+symbol+"Title"+symbol+"\n"+symbol*9
     return output
